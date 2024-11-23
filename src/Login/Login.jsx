53c8c707abb9735f3caa12f1b03acc5ac5ea64b2
JJungMing <cma400195@gmail.com>
2024-11-24 03:50:46 +0900

    login update

--- src/Login/Login.jsx
+++ src/Login/Login.jsx
@@ -1,11 +1,16 @@
 import React from "react";
 import styles from "./Login.module.css";
-import { useNavigate } from "react-router-dom";
 import PeopleIcon from "../images/image.png";
 import KakaoLogin from "../images/kakao_login.png"; 
 
 function Login() {
-    const navigate = useNavigate();
+    const REST_API_KEY = 'd67abb59691d4f30320c647af9334ce9';
+    const REDIRECT_URI = 'https://klay-ten.vercel.app/api/login/after';
+    const link = `https://kauth.kakao.com/oauth/authorize?client_id=${REST_API_KEY}&redirect_uri=${REDIRECT_URI}&response_type=code`;
+      
+    const loginHandler = () => {
+      window.location.href = link;
+    }
 
     return (
         <div className={styles.frame}>
@@ -19,7 +24,7 @@ function Login() {
                 <p className={styles.text1}>Welcome to Klay!</p>
                 <p className={styles.text2}>Sign in with Kakao</p>
                 <img src={KakaoLogin} className={styles.login_button} alt="Kakao Login" 
-                    onClick={() => navigate("/home")}
+                    onClick={loginHandler}
                 />
             </div>
         </div>

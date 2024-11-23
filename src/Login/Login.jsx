07d4bed5ff7b5f831a1cd62258438a61a22b172b
JJungMing <cma400195@gmail.com>
2024-11-24 00:37:16 +0900

    login update

--- src/Login/Login.jsx
+++ src/Login/Login.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import styles from "./Login.module.css";
 import { useNavigate } from "react-router-dom";
-import { RiKakaoTalkFill } from "react-icons/ri";
-import PeopleIcon from "../images/image.png"; 
+import PeopleIcon from "../images/image.png";
+import KakaoLogin from "../images/kakao_login.png"; 
 
 function Login() {
     const navigate = useNavigate();
@@ -18,10 +18,9 @@ function Login() {
             <div className={styles.welcome_container}>
                 <p className={styles.text1}>Welcome to Klay!</p>
                 <p className={styles.text2}>Sign in with Kakao</p>
-                <button className={styles.login_button} onClick={() => navigate("/home")}>
-                    <RiKakaoTalkFill /> 
-                    <p> 카카오 로그인</p>
-                </button>
+                <img src={KakaoLogin} className={styles.login_button} alt="Kakao Login" 
+                    onClick={() => navigate("/home")}
+                />
             </div>
         </div>
     );
